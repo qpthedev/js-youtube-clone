c4be57e37ec735c59bf7bae8b191ddc8c5053e3d
qpthedev <qpthedev@gmail.com>
2020-06-18 03:09:49 -0400

    [Add] Added route for join page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.render("logout");
+export const join = (req, res) => res.render("join");
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
 export const changePassword = (req, res) => res.render("changePassword");
