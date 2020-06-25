2ad82cee842b0fad50f6ceb5a20cf9d7aae43e2c
qpthedev <qpthedev@gmail.com>
2020-06-25 05:43:58 -0400

    [Add] Fake DB for case testing

---
+++ db.js
@@ -0,0 +1,41 @@
+export const videos = [
+  {
+    id: 33221,
+    title: "Awesome Video",
+    description: "This is an awesome video",
+    views: 42,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 112445,
+      name: "qpthedev",
+      email: "qpthedev@dev.com",
+    },
+  },
+  {
+    id: 46712677,
+    title: "Super Awesome Video",
+    description: "This is an awesome video",
+    views: 422,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 16797,
+      name: "qpthedev",
+      email: "qpthedev@dev.com",
+    },
+  },
+  {
+    id: 128798865,
+    title: "Super Duper Awesome Video",
+    description: "This is an awesome video",
+    views: 4112,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 87088664,
+      name: "qpthedev",
+      email: "qpthedev@dev.com",
+    },
+  },
+];
