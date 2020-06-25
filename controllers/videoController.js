810d27d4cd9e7c27fbb2054d9a141dddd5d1a9ff
qpthedev <qpthedev@gmail.com>
2020-06-25 05:45:53 -0400

    [Add] Code up to current progress

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,17 +1,35 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import { videos } from "../db";
+import routes from "../routes";
+
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
+
 export const search = (req, res) => {
   // const searchingBy = req.query.term;
   const {
     query: { term: searchingBy },
   } = req;
   // console.log(searchingBy);
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
-export const upload = (req, res) =>
+
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description },
+  } = req;
+  // To do: upload and save video
+  res.redirect(routes.videoDetail(12345));
+};
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
