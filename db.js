e8ac901168a7977251c05e4c13717c11cd1b9fdd
qpthedev <qpthedev@gmail.com>
2020-06-30 04:18:54 -0400

    [Add] Added Video Model for MongoDB

--- db.js
+++ db.js
@@ -1,41 +1,22 @@
-export const videos = [
-  {
-    id: 33221,
-    title: "Awesome Video",
-    description: "This is an awesome video",
-    views: 42,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 112445,
-      name: "qpthedev",
-      email: "qpthedev@dev.com",
-    },
-  },
-  {
-    id: 46712677,
-    title: "Super Awesome Video",
-    description: "This is an awesome video",
-    views: 422,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 16797,
-      name: "qpthedev",
-      email: "qpthedev@dev.com",
-    },
-  },
-  {
-    id: 128798865,
-    title: "Super Duper Awesome Video",
-    description: "This is an awesome video",
-    views: 4112,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 87088664,
-      name: "qpthedev",
-      email: "qpthedev@dev.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  // useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => {
+  console.log(`✅Connected to DB`);
+};
+
+const handleError = (error) => {
+  console.log(`❌Error on DB Connection: ${error}`);
+};
+
+db.once("open", handleOpen);
+db.on("error", handleError);
