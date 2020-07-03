30fbf22ab010e81735fab28035be53a693f1db90
qpthedev <qpthedev@gmail.com>
2020-07-03 04:29:00 -0400

    [Add] Add code to progress

---
+++ controllers/userController.js
@@ -0,0 +1,41 @@
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To do: register user
+    // To do: log user in
+    res.redirect(routes.home);
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  // To Do: process log out
+  // res.render("logout", { pageTitle: "Logout" });
+  res.redirect(routes.home);
+};
+
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
+
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
