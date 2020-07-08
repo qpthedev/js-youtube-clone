af5da94eac2c125dc3c878a71a4c75cfd988f620
qpthedev <qpthedev@gmail.com>
2020-07-08 03:18:18 -0400

    [Add] Added edit for passport authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,12 @@
+import passport from "passport";
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 },
   } = req;
@@ -13,17 +15,29 @@ export const postJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
   } else {
     // To do: register user
-    // To do: log user in
-    res.redirect(routes.home);
+    try {
+      const user = await User({
+        name,
+        email,
+      });
+
+      await User.register(user, password);
+      // To do: log user in
+      next();
+    } catch (error) {
+      console.log(error);
+      res.redirect(routes.home);
+    }
   }
 };
 
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
 
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home,
+});
 
 export const logout = (req, res) => {
   // To Do: process log out
