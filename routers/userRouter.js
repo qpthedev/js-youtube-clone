c61050c2c08a2b5f3c24935611c5cd293bfd2cec
qpthedev <qpthedev@gmail.com>
2020-07-13 02:13:13 -0400

    [Add] edit scss for user profile page

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,16 +1,15 @@
 import express from "express";
 import routes from "../routes";
 import {
-  users,
   userDetail,
-  editProfile,
   changePassword,
+  getEditProfile,
 } from "../controllers/userController";
 import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
