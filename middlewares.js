ffcdd5603969aa488ca6e2b572f767844346a27b
qpthedev <qpthedev@gmail.com>
2020-07-13 05:37:27 -0400

    [Add] add multer for user avatar update

--- middlewares.js
+++ middlewares.js
@@ -2,6 +2,7 @@ import multer from "multer";
 import routes from "./routes";
 
 const multerVideo = multer({ dest: "uploads/videos/" });
+const multerAvatar = multer({ dest: "uploads/avatars/" });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
@@ -25,4 +26,6 @@ export const onlyPrivate = (req, res, next) => {
     res.redirect(routes.home);
   }
 };
+
 export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
