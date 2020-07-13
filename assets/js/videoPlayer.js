970339ad70dc04ed7bf70be36f0353590ee0cc27
qpthedev <qpthedev@gmail.com>
2020-07-13 05:38:26 -0400

    [Add] edit video player, controls, and scss

---
+++ assets/js/videoPlayer.js
@@ -0,0 +1,20 @@
+const videoContainer = document.getElementById("jsVideoPlayer");
+const videoPlayer = document.querySelector("#jsVideoPlayer video");
+const playButton = document.getElementById("jsPlayButton");
+// let videoPlayer;
+
+function handlePlayClick() {
+  if (videoPlayer.paused) {
+    videoPlayer.play();
+  } else {
+    videoPlayer.pause();
+  }
+}
+
+function init() {
+  playButton.addEventListener("click", handlePlayClick);
+}
+
+if (videoContainer) {
+  init();
+}
