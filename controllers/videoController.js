498768d535af5705e2f74373ddcbe6673ab38b0c
qpthedev <qpthedev@gmail.com>
2020-07-21 06:46:30 -0400

    [Add] Added AWS S3 and multer s3

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -39,18 +39,16 @@ export const getUpload = (req, res) =>
 export const postUpload = async (req, res) => {
   const {
     body: { title, description },
-    file: { path },
+    file: { location },
   } = req;
 
   const newVideo = await Video.create({
-    fileUrl: path,
+    fileUrl: location,
     title: title,
     description: description,
     creator: req.user.id,
   });
 
-  // console.log(newVideo);
-  // res.render("upload", { pageTitle: "Upload" });
   req.user.videos.push(newVideo.id);
   req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
